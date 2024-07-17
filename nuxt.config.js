e70dc882099404e2cea7c53170e51392ee42264e
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2024-07-18 03:22:31 +0800

    update router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,8 @@ export default {
     { src: "@/plugins/utils/util.ts", mode: "client", ssr:false },
   ],
   router:{
-    base: '/TKFM-Data-Room',
-    // base: '/',
+    // base: '/TKFM-Data-Room',
+    base: '/',
   },
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
