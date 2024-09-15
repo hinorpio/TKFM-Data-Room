4203bd8022a1f685f224ef568a35d973409c9e76
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2024-09-15 13:43:08 +0800

    update unit select box image

--- components/Unit/UnitTab/OtherVersionTab.vue
+++ components/Unit/UnitTab/OtherVersionTab.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-row class="mt-4">
+    <v-row class="mt-4 px-1">
         <v-img 
             v-for="(form, index) in handleGetOtherVersion()" 
             :key="index" 
@@ -15,14 +15,41 @@
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import { Unit } from '@/interface/unit'
+import { writeFileSync } from 'fs';
 
 @Component
 export default class OtherVersionTab extends Vue {
     @Prop({ type: Object, required: true, default: {} })
     unit!: Unit;
 
+    mounted(){
+        // this.generateList()
+    }
+
+    generateList(): void {
+        var data = this.$util.getAllUnitData()
+        var exportdata = data.map(f => {
+            return {
+                ID: f.ID,
+                rarity: f.rarity,
+                element: f.element,
+                position: f.position
+            }
+        })
+        const blob = new Blob([JSON.stringify(exportdata, null, 2)], {
+            type: 'application/json',
+        });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `selection_pic_handle.json`;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+    
+
     get imgWidth(): string {
-        return this.$util.getValueByBreakPoint(this.$vuetify.breakpoint.name, '20%', '10%', '15%', '12.5%', '10%')
+        return this.$util.getValueByBreakPoint(this.$vuetify.breakpoint.name, '20%', '10%', '15%', '12.5%', '9.5%')
     }
 
     handleSelect(unit: Unit): void {
