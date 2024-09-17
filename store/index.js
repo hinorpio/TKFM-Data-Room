43a77d7c446c3de94ab09d95b2aea1a9f4dfe9da
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2024-09-18 03:15:43 +0800

    init beta 0.1.0

--- store/index.js
+++ store/index.js
@@ -7,7 +7,7 @@ Vue.use(Vuex);
 const store = () => new Vuex.Store({
     state: {
       themeColor: '#5f9df7',
-      version: 'beta-0.0.12'
+      version: 'beta-0.1.0'
     },
     mutations: {
       // Define your mutations here
