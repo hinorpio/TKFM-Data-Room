cf1ccda52e77a9dc70e05d3e18c93a86c8e44283
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2025-03-14 01:30:55 +0800

    add fanart

--- static/data/arts/fanart/DOKGUN_OLD_STICKER.ts
+++ static/data/arts/fanart/DOKGUN_OLD_STICKER.ts
@@ -2,19 +2,19 @@ import { Locale, ArtSource, ArtType } from '@/plugins/utils/enums';
 import { Art } from '~/interface/art';
 import { ArtFile } from '~/static/const';
 
-const DOKGUN_STICKER_KR: Art = {
-    code: 'DOKGUN_STICKER_KR',
+const DOKGUN_OLD_STICKER: Art = {
+    code: 'DOKGUN_OLD_STICKER',
     source: ArtSource.FANART,
     type: ArtType.STICKER,
     locale: [ Locale.kr ],
     author: '독군 Dokgun',
     authorLink: 'https://www.pixiv.net/users/19278065',
     name: {
-        [Locale.tc]: '독군 Dokgun - GIF 貼圖 (韓文)',
-        [Locale.sc]: '독군 Dokgun - GIF 贴图 (韓文)',
-        [Locale.en]: '독군 Dokgun - GIF Sticker (Korean)',
-        [Locale.jp]: '독군 Dokgun - GIF スタンプ (韓国語)',
-        [Locale.kr]: '독군 Dokgun - GIF 스티커 (한국어)',
+        [Locale.tc]: '독군 Dokgun - 貼圖 V1 (韓文)',
+        [Locale.sc]: '독군 Dokgun - 贴图 V1 (韓文)',
+        [Locale.en]: '독군 Dokgun - Sticker V1 (Korean)',
+        [Locale.jp]: '독군 Dokgun - スタンプ V1 (韓国語)',
+        [Locale.kr]: '독군 Dokgun - 스티커 V1 (한국어)',
     },
     remark: {
         [Locale.tc]: '*已取得作者 독군 Dokgun 授權轉載',
@@ -23,7 +23,7 @@ const DOKGUN_STICKER_KR: Art = {
         [Locale.jp]: '*作者 독군 Dokgun の許可を得て転載しました',
         [Locale.kr]: '*독군 Dokgun 작가의 허가를 받아 재게시했습니다',
     },
-    paths: ArtFile.FANART.DOKGUN_STICKER_KR,
+    paths: ArtFile.FANART.DOKGUN_OLD_STICKER,
 }
 
-export default DOKGUN_STICKER_KR
+export default DOKGUN_OLD_STICKER
