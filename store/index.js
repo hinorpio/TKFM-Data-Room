dc394ae0ae64d7e7d9f5e7e10a561781aecf8dab
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2025-07-16 17:33:34 +0800

    beta 0.6.2

--- store/index.js
+++ store/index.js
@@ -7,7 +7,7 @@ Vue.use(Vuex);
 const store = () => new Vuex.Store({
     state: {
       themeColor: '#5f9df7',
-      version: 'beta-0.6.1'
+      version: 'beta-0.6.2'
     },
     mutations: {
       // Define your mutations here
